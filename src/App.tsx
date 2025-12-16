0403dab5d1883ac62669306a2bb6b9ad59ae69b9
RaviMusalin <sheikhmusalin@gmail.com>
2025-12-16 12:58:43 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-3xl font-bold">
+        FinalSpotify Rebuild
+      </h1>
+    </div>
+  );
+}
+
