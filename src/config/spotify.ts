f836fad0f4418949ebf79d11a0d924b78ffbdbdb
RaviMusalin <sheikhmusalin@gmail.com>
2025-12-20 11:12:22 -0500

    Spotify config created

---
+++ src/config/spotify.ts
@@ -0,0 +1,8 @@
+export const SPOTIFY_AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+
+export const SPOTIFY_SCOPES = [
+  "playlist-modify-public",
+  "playlist-modify-private",
+];
+
+export const REDIRECT_URI = "http://localhost:5173/";
